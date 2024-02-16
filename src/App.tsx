9c83a9e4c910d19cae543efad58cc5268a384f04
Ankit Kumain <ankit.kumain@medfin.in>
2024-02-16 09:28:37 +0530

    refactor: use web sdk instead of manual PKCE method

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@
 import './App.css';
 import { useQuery } from '@tanstack/react-query';
 import { getPlaylistFromPrompt } from './services/getPlaylist';
-import { useSpotify } from './hooks/useSpotify';
-import { Scopes, SearchResults, SpotifyApi } from './utils/spotify/';
+import { SpotifyApi, Scopes } from '@spotify/web-api-ts-sdk';
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  // const [tracks, setTracks] = useState<string[]>([]);
+  const [tracks, setTracks] = useState<string[]>([]);
 
   const { isPending, error, data } = useQuery({
     queryKey: ['repoData'],
@@ -18,7 +18,6 @@ function App() {
       console.log(data)
       if (typeof data === 'string') {
         const parsedData = JSON.parse(data);
-        // setTracks(parsedData);
         if (Array.isArray(parsedData)) {
           return parsedData.map(track => `${track.song} by ${track.artist}`);
         }
@@ -27,11 +26,34 @@ function App() {
     }
   })
 
-  const sdk = useSpotify(
-    import.meta.env.VITE_SPOTIFY_CLIENT_ID, 
-    import.meta.env.VITE_REDIRECT_TARGET, 
-    Scopes.userDetails
-  );
+  const sdk = SpotifyApi.withClientCredentials(import.meta.env.VITE_SPOTIFY_CLIENT_ID, import.meta.env.VITE_SPOTIFY_SECRET_ID,  Scopes.all);
+  useEffect(()=>{
+
+    (async function(){try{
+
+      console.log("-->", sdk)
+
+      const items = await sdk.search("City Light by Raghav Meatle", ["track"]);
+  
+      console.table(items.tracks.items.map((item) => ({
+          name: item.name,
+          trackId: item.id,
+          popularity: item.popularity,
+      })));
+
+      
+    }catch(e){
+      console.log(e);
+    }})()
+    
+  },[])
+  
+
+  // const sdk = useSpotify(
+  //   import.meta.env.VITE_SPOTIFY_CLIENT_ID, 
+  //   import.meta.env.VITE_REDIRECT_TARGET, 
+  //   Scopes.userDetails
+  // );
 
 
   console.log(sdk);
